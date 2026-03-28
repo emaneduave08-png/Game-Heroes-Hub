b9410f125db5e5749952a006e3329ed21449b97b
EMANUEL <emaneduave08@gmail.com>
2026-03-28 14:10:48 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-function showMLHero() {
-  document.getElementById("mlOutput").innerHTML =
-    "<h2>Alucard</h2>" +
-    "<p>Role: Fighter</p>" +
-    "<p>Strategy: Mag-farm muna, then engage kapag may items na para sa lifesteal sustain.</p>";
-}
-function showHOKHero() {
-  document.getElementById("hokOutput").innerHTML =
-    "<h2>Lu Bu</h2>" +
-    "<p>Role: Fighter</p>" +
-    "<p>Strategy: Mag-engage kapag may ult, very strong sa 1v1 at team fights.</p>";
-}
-function searchML() {
-  let input = document.getElementById("searchML").value.toLowerCase();
-
-  if (input === "alucard") {
-    showMLHero();
-  }
-}
-function searchHOK() {
-  let input = document.getElementById("searchHOK").value.toLowerCase();
-
-  if (input === "lu bu") {
-    showHOKHero();
-  }
-}
 const mlHeroes = [
   {
     name: "Alucard",
